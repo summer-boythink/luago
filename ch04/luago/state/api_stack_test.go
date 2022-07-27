cafc21cdf65fecd528b13f6e44dd0c0ac58b4d04
summer-boythink <2071792205@qq.com>
2022-07-27 19:52:04 +0800

    stack api

---
+++ ch04/luago/state/api_stack_test.go
@@ -0,0 +1,20 @@
+package state
+
+import (
+	"reflect"
+	"testing"
+)
+
+var l *luaState = &luaState{
+	&luaStack{
+		slots: []luaValue{'a','b','c','d','e'},
+		top:5,
+	},
+}
+
+func TestRotate(t *testing.T) {
+	l.Rotate(2,1)
+	if !reflect.DeepEqual(l.stack.slots,[]luaValue{'a','e','b','c','d'}){
+		t.Error("err")
+	}
+}
